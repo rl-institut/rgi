7e877099e63a963178f2ec5d22025e378d8f877a
daniel.rdt <daniel.ruedt@campus.tu-berlin.de>
2023-07-26 16:52:57 +0200

    added offshore with uniform color and with hover info only node offshore

--- graphs.py
+++ graphs.py
@@ -1,5 +1,5 @@
 """Holds functionality for plotly graphs."""
-
+import numpy as np
 import pandas as pd
 from plotly import express as px
 from plotly import graph_objects as go
@@ -60,6 +60,8 @@ def get_choropleth(
     )
     # add pretty name for hovering box
     df["pretty_name"] = df.name.str[:3]
+    df["offshore_color"] = np.repeat("offshore", len(df))
+
     geojson = data.get_regions()
     geojson_offshore = data.get_regions_offshore()
 
@@ -79,18 +81,23 @@ def get_choropleth(
     fig2 = px.choropleth(
         df,
         geojson=geojson_offshore,
-        locations="name",
-        color=unit,
+        locations='name',
+        color="offshore_color",
+        color_discrete_map={
+            'offshore': '#8AC7DB'},
         # opacity=0.1,
         featureidkey="properties.name",
-        hover_name="pretty_name",
-        hover_data={"name": False, unit: True},
+        hover_name=df.pretty_name + " Offshore",
+        hover_data={"name": False, unit: False, "offshore_color": False},
         labels=pretty_labels,
     )
+
+    # fig2.update_layout(showlegend=False)
 
     fig.add_trace(fig2.data[0])
 
     fig.update_layout(
+        showlegend=False,
         margin={"l": 0, "r": 0, "b": 0, "t": 75},
         font_family=FONT,
         title={"text": title},
@@ -98,6 +105,7 @@ def get_choropleth(
     )
 
     fig.update_geos(
+        scope="europe",
         fitbounds="locations",
         visible=False,
         resolution=50,
@@ -150,7 +158,12 @@ def get_bar_chart(  # noqa: PLR0913
         font_family=FONT,
         plot_bgcolor="rgba(0,0,0,0)",
         margin=dict(t=75),
-        # legend={"orientation": "h"},
+        legend=dict(
+            traceorder="reversed",
+            bgcolor="LightSteelBlue",
+            bordercolor="Black",
+            borderwidth=2
+        ),
         hoverlabel={"bgcolor": "white", "font_size": 12, "font_family": FONT}
     )
 
