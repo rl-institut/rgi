646b9bd932cfcc472a16f06c3a18ff77d59cd2dc
Maike Held <Maike.Held@rl-institut.de>
2023-08-12 18:35:57 +0200

    change title of bar chart; add new PAC2_0 results

--- graphs.py
+++ graphs.py
@@ -247,7 +247,7 @@ def get_bar_chart(  # noqa: PLR0913
     )
 
     # change annotation above graph to only show scenario name; optional: give text="" for no annotation
-    fig.for_each_annotation(lambda a: a.update(text=f"{a.text[9:].upper()}: {region}"))
+    fig.for_each_annotation(lambda a: a.update(text=f"{a.text[9:].upper()}: {data.get_pretty_names()[region]}"))
     # set fixed bar width
     fig.update_traces(width=0.08)
 
