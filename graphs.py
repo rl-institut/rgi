5148bb71d515fa58594f4fceedae0df3e9dfe351
birgits <birgit.schachler@rl-institut.de>
2023-09-25 11:13:55 +0200

    Exclude region from bar plot title
    dieser

--- graphs.py
+++ graphs.py
@@ -277,8 +277,7 @@ def get_bar_chart(  # noqa: PLR0913
     # change annotation above graph to only show scenario name; optional: give text="" for no annotation
     fig.for_each_annotation(lambda a:
                             a.update(
-                                text=f"{data.get_sce_pretty_names_2()[a.text[9:]]}:"
-                                     f" {data.get_pretty_names()[region]}"))
+                                text=f"{data.get_sce_pretty_names_2()[a.text[9:]]}"))
     fig.update_annotations(font_size=18)
     # set fixed bar width
     fig.update_traces(width=0.08)
